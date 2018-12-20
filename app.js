cd39858396efa3d492b4b22fc053e23eada2e7aa
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-12-20 15:55:42 -0400

    Partial setup of module controllers

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 let budgetController = ( () => {
-  let x = 23;
-  let add = (a) => {
-    return x + a;
-  }
 
-  return {
-    publicTest: (b) => {
-      return add(b);
-    }
-  }
 })();
 
 let UIController = (() => {
+  let DOMstrings = {
+    inputType: '.add__type',
+    inputDesc: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  }
 
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        desc: document.querySelector(DOMstrings.inputDesc).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDomStrings: () => {
+      return DOMstrings;
+    }
+  }
 })();
 
 let controller = ((budgetCtrl, UICtrl) => {
-  let z = budgetCtrl.publicTest(5);
+  let setupEventListeners = () => {
+    let DOM = UICtrl.getDomStrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', (event) => {
+      if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+    }
+  });
+  };
+
+  let ctrlAddItem = () => {
+    //Get field input data
+    let input = UICtrl.getInput();
+    console.log(input);
+
+    //Add item to budget controller
+    //Add new item to UI
+    //Calculate the budget
+    //Display budget on UI
+
+  }
 
   return {
-    anotherPublic: () => {
-      console.log(z);
+    init: () => {
+      console.log('Application has started.');
+      setupEventListeners();
     }
   }
 
+
 })(budgetController, UIController);
+
+controller.init();
